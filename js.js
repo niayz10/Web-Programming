e199212cfea60ee2bb515d59c7f846d3952f09cf
niayz10 <43026825+niayz10@users.noreply.github.com>
2020-02-07 13:13:43 +0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,36 @@
+
+let box = document.querySelector('.box');
+let todo_input = document.querySelector('#in1');
+
+
+function addItem(){  
+    let div=document.createElement('div');
+    let item = document.createElement('input');
+    let item2 = document.createElement('label');
+    let item3=document.createElement('img');
+    item3.onclick=function(){
+        div.remove();
+    };
+    item3.id="del";
+    div.appendChild(item);
+    div.appendChild(item2);
+    div.appendChild(item3);
+    div.id="div";
+    div.style.height="40px";
+    item.type="checkbox";
+    item.id = "asd";
+    item2.style.position="relative";
+    item2.style.left="30px";
+    item2.style.top="-20px";
+    item.style.display="grid";
+    box.style.visibility="visible";
+    item3.src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTCEZi5RM9OxlV-KOKModNC0w9oqjdPVM1qzVced9SXj4xnM_a0"
+    item3.style.height="30px";
+    item3.style.position='absolute';
+    item3.style.left="617px";
+    item3.style.top="4px";
+    if(todo_input.value != ''){
+      item2.innerHTML = todo_input.value;
+     box.appendChild(div);
+  }
+}
